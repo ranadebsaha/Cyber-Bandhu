d55a5b7004152740509c938a5e07bebec99bdd1e
ranadebsaha <ranadebsaha2002@gmail.com>
2025-04-18 23:56:01 +0530

    clerk removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,9 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+
 
 export default function App() {
   return (
-    <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <div>
+      hello
+    </div>
   );
 }
